95ed52db55c7453c296f2dd142d745c73bd1dbd8
Vaibhav Kubre <v@kubre.in>
2023-09-23 19:07:46 +0530

    Alot of progress

--- scripts/Bow.gd
+++ scripts/Bow.gd
@@ -1,31 +1,35 @@
+@icon("res://sprites/player.png")
+
 extends Node2D
 
+class_name Player
 
-var arrow_spawn_point: Vector2
-var Arrow := preload("res://assests/Arrow.tscn")
-signal arrow_fired
+const ArrowScn := preload("res://assests/Projectile.tscn")
 var arrows_left = true
+
+signal arrow_fired
+
 
 func _ready() -> void:
 	connect("arrow_fired", Callable(get_tree().root.get_node("World"), "arrow_fired"))
 
+
 func _physics_process(_delta: float) -> void:
-	look_at(get_global_mouse_position())
+	$Cannon.look_at(get_global_mouse_position())
 
-	if Input.is_action_just_pressed("shoot"):
-		$Sprite.frame = 1
-	elif Input.is_action_just_released("shoot"):
-		$Sprite.frame = 0
-		fire(Arrow)
+	if Input.is_action_just_released("shoot"):
+		fire(ArrowScn)
 
-func fire(projectile)-> void:
+
+func fire(projectile: PackedScene) -> void:
 	if not arrows_left:
 		return
-	var arrow = projectile.instantiate()
+	var arrow: Projectile = projectile.instantiate()
 	get_tree().root.add_child(arrow)
 	arrow.connect("balloon_hit", Callable(get_tree().root.get_node("World"), "balloon_hit"))
-	arrow.start(transform)
+	arrow.start($Cannon.get_global_transform())
 	emit_signal("arrow_fired")
 
-func are_arrow_left(_arrows_left)-> void:
+
+func are_arrow_left(_arrows_left) -> void:
 	arrows_left = _arrows_left
